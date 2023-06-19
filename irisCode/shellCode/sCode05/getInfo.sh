55bf24fd2d74c6dbd4e94afabce1e7e997f2a7ae
iris <blossemiris@gmail.com>
2023-06-19 22:54:25 +0000

    get value from cmd

---
+++ irisCode/shellCode/sCode05/getInfo.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# author Iris Wu
+# date 06/20/2023
+
+echo "You will get args from the user";
+echo "There are $# values:";
+echo "The values are $*";
+echo "The name of the file is $0"
+echo "The first one is $1";
+echo "The second one is $2";
+echo "The third one is $3";
